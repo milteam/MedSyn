af30491383b362800b2f19599eebd25842820e7f
Nikolay <aby2sz@gmail.com>
2023-06-20 12:50:04 +0300

    congig.json

--- scripts/generate/medalpaca.py
+++ scripts/generate/medalpaca.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
-qa_pipeline = pipeline("question-answering", model="medalpaca/medalpaca-lora-7b-8bit", tokenizer="medalpaca/medalpaca-lora-7b-8bit", config="medalpaca/medalpaca-lora-7b-8bit/adapter_config.json")
+qa_pipeline = pipeline("question-answering", model="medalpaca/medalpaca-lora-7b-8bit", tokenizer="medalpaca/medalpaca-lora-7b-8bit", config="./ma7b8conf")
 question = "What are the symptoms of diabetes?"
 context = "Diabetes is a metabolic disease that causes high blood sugar. The symptoms include increased thirst, frequent urination, and unexplained weight loss."
 answer = qa_pipeline({"question": question, "context": context})
