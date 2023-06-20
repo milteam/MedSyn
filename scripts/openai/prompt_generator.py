a5a79f388766e9f049cb4af3eec1788ae0a8d14a
Nikolay <aby2sz@gmail.com>
2023-06-20 14:52:58 +0300

    prompts in english

--- scripts/openai/prompt_generator.py
+++ scripts/openai/prompt_generator.py
@@ -1,10 +1,13 @@
 import json
+import random
+
+import click
 
 from graph import DiseaseInfo
 
 
 
-def generate_prompt(info: DiseaseInfo):
+def generate_prompt_ru(info: DiseaseInfo):
     return [{"role": "user", "content": f'''
             Напиши ответ как будто являешься профессиональным врачом, который записывает анамнез со слов пациента. Напиши хорошо структурированный и детальный отчет.
 Представь, что у пациента есть слеующие хронические заболевания: {", ".join(info.preconditions)}.
@@ -12,12 +15,24 @@ def generate_prompt(info: DiseaseInfo):
 Пациент жалуется на {", ".join(info.symptoms)}. Реалистично придумай все недостающие данные для отчета.'''},
             ]
 
+def generate_prompt_en(data):
+    actual_desease = data["disease"][0]["Name"]
+    symptoms = data["symptoms"]
+    if len(symptoms) > 3:
+        symptoms = random.sample(symptoms, random.randint(1,4))
+    return f'''
+    Act like a professional doctor who listened to the patient. Write a well-structured and extremely detailed medical anamnesis that includes all the required sections.        
+  In fact, the patient has {actual_desease}, but the doctor does not know about it.
+  The patient complains of {", ".join(symptoms)}.
+  '''
 
 @click.command()
-@click.option('--taskid', '-t', help='Task id', multiple=True, required=True, type=int)
-def main():
-    pass
-
+@click.option('--samples', '-s', help='JSON files witj deseases', required=True)
+def main(samples):
+    with open(samples, encoding="utf8") as f:
+        data = json.load(f)
+        for idx, desease in data.items():
+            print(generate_prompt_en(desease))
 
 if __name__ == '__main__':
     main()
