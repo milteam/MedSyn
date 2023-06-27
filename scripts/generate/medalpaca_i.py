647beb467f91086171e0f514b0a849f45144c9ee
nikolai <aby2sz@gmail.com>
2023-06-27 14:32:07 +0300

    reqs

--- scripts/generate/medalpaca_i.py
+++ scripts/generate/medalpaca_i.py
@@ -6,8 +6,10 @@ assert torch.cuda.is_available(), "No cuda device detected"
 inferer = load_model()
 
 print(inferer('''
-Write a well-structured  anamnesis that includes all the required sections.
+Act like a professional doctor who listened to the patient. Write a well-structured and extremely detailed medical anamnesis that includes all the required sections.
+The patient has chronic diseases: gastritis, tonsillitis.
 In fact, the patient has an exacerbation of chronic gastritis, but the doctor does not know about it.
 The patient complains of stomach pain and vomiting.
-''',
-              instruction="Act as a professional doctor"))
+Mark up the text: highlight all the symptoms and medical terms in square brackets, write down the type of the word
+
+'''))
