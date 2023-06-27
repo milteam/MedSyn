6f8968aa39ce31aa9184f2cc011845c9c5970e1f
nikolai <aby2sz@gmail.com>
2023-06-27 10:16:29 +0300

    reqs

--- scripts/generate/medalpaca_i.py
+++ scripts/generate/medalpaca_i.py
@@ -9,4 +9,5 @@ print(inferer('''
 Write a well-structured  anamnesis that includes all the required sections.
 In fact, the patient has an exacerbation of chronic gastritis, but the doctor does not know about it.
 The patient complains of stomach pain and vomiting.
-'''))
+''',
+              instruction="Act as a professional doctor"))
