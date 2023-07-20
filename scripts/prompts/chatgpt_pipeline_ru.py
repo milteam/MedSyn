b2979e4d77bfc4a08b7a79d6ac850068532808c4
nikolai <aby2sz@gmail.com>
2023-07-20 14:35:16 +0300

    chatgpt ru pipeline

--- scripts/prompts/chatgpt_pipeline_ru.py
+++ scripts/prompts/chatgpt_pipeline_ru.py
@@ -1,10 +1,38 @@
 import json
 import os
+import random
 import time
 import openai
 import click
 from openai.error import ServiceUnavailableError, OpenAIError
 
+
+def prompt_no_patent_info(symptoms, diagnose, marital, smoking, gender, conj ):
+    return f'''
+    Напиши ответ как будто являешься профессиональным врачом, который записывает анамнез со слов пациента. Напиши хорошо структурированный и детальный анамнез.
+    Пациент жалуется на {symptoms}.
+    Реалистично придумай все недостающие детали, характеристики пациента и сопутствующие симпотмы для анамнеза. '''
+
+
+def prompt_with_patient_info(symptoms, diagnose, marital, smoking, gender, conj ):
+    return f'''
+Напиши текст анамнеза, составленного врачом по итогам приема пациента.
+Пациент - {marital} {smoking} {gender}, {conj} жалуется на {symptoms}.
+Придумай все недостающие детали, характеристики пациента и сопутствующие симпотмы для анамнеза. '''
+
+
+def short_promt(symptoms, diagnose, marital, smoking, gender, conj ):
+    return f'''
+Представь, что являешься врачом. Напиши краткий анамнез по итогам приема пациента, который жалуется на {symptoms}.
+ Дополни все недостояющая детали реалистичными данными. '''
+
+def prompt_diagnostics(symptoms, diagnose, marital, smoking, gender, conj ):
+    return f'''
+    Напиши ответ как будто являешься профессиональным врачом, который записывает анамнез со слов пациента. Напиши хорошо структурированный и детальный анамнез.
+    Пациент жалуется на {symptoms}.
+    Реалистично придумай все недостающие детали и результаты исследований, которые могли быть назначены в такой ситуации. '''
+
+PROMPTS = [prompt_diagnostics]
 
 def get_sample(data):
     desease_code = data["disease"][0]["idc_10"]
@@ -13,24 +41,25 @@ def get_sample(data):
     gender = data["gender"]
     marital_state = data["family_state"]
     smoking = data["smoking"]
+
+    desaese_name = (data["disease"][0]["name_ru"] or "").lower()
     if gender == "male":
         marital = "замужний" if marital_state else "незамужний"
         smoking = "курящий" if smoking else "некурящий"
         gender_ru = "мужчина"
+        conj = "который"
     else:
         marital = "замужняя" if marital_state else "незамужняя"
         smoking = "курящая" if smoking else "некурящая"
         gender_ru = "женщина"
+        conj = "которая"
         #Известно, что пациент - {marital} {smoking} {gender_ru}.
     # if len(symptoms) > 3:
     #     symptoms = random.sample(symptoms, random.randint(1,4))
     return dict(desease_code=desease_code, symptoms=symptoms, gender=gender, marital_state=marital_state, smoking=smoking,
-                prompt=f'''
-Напиши ответ как будто являешься профессиональным врачом, который записывает анамнез со слов пациента. Напиши хорошо структурированный и детальный отчет.
-Пациент жалуется на {", ".join(symptoms).lower()}. 
-Реалистично придумай все недостающие данные для отчета как если бы у пациента был {data["disease"][0]["name_ru"].lower()}. 
-Диагноз не должен быть написан в анамнезе.
-Фраза "{data["disease"][0]["name_ru"].lower()}" не должна упоминаться в ответе ни в каком виде.''')
+                desease_name=desaese_name,
+                prompt=random.choice(PROMPTS)(", ".join(symptoms).lower(), desaese_name,
+                                              marital, smoking, gender_ru, conj))
 #На момент написания анамнеза диагноз пациента не известен и не должен быть упомянут в ответе.
 
 
@@ -41,6 +70,8 @@ def get_sample(data):
 @click.option('--limit', '-l', help='A number of sentences to generate', default=100)
 @click.option('--offset', '-o', help='A number of sentences to skip', default=0)
 def main(dir, samples, limit, offset):
+    random.seed(0)
+
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     tic = time.perf_counter()
@@ -64,6 +95,8 @@ def main(dir, samples, limit, offset):
 
                 print(f"\n\n\n======================== Sample {idx+1} ========================")
                 desease_info = get_sample(data[idx])
+                print(desease_info["desease_name"])
+                print("\n\n")
                 print(desease_info["prompt"])
                 print("-----------------------------------------------------------------")
                 response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": desease_info["prompt"]}])
