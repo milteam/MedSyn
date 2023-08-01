e4f57004b382fe0b80477ea47e661db0a99386ab
glebkaa <iamglebk@gmail.com>
2023-08-01 07:42:03 +0300

    codes preparation bug fix

--- scripts/sampler/prepare_codes.py
+++ scripts/sampler/prepare_codes.py
@@ -6,7 +6,16 @@ import pandas as pd
 
 
 def prepare_codes(cfg: Dict) -> None:
-    codes = pd.read_csv(cfg["icd_codes"])
+    """Add child categories for top_ICD10_codes.
+
+    Data source for icd_codes:
+        https://github.com/k4m1113/ICD-10-CSV/tree/master
+        https://www.cms.gov/Medicare/Coding/ICD10/2018-ICD-10-CM-and-GEMs
+
+    Args:
+        cfg (Dict): _description_
+    """
+    codes = pd.read_csv(cfg["icd_codes"], header=None)
     codes.columns = [
         "Category_Code",
         "Diagnosis_Code",
@@ -15,6 +24,8 @@ def prepare_codes(cfg: Dict) -> None:
         "Full_Description",
         "Category_Title",
     ]
+    codes["Primary_code"] = codes["Full_Code"].apply(lambda x: x[:3])
+    codes["Secondary_code"] = codes["Full_Code"].apply(lambda x: x[3:])
 
     with open(cfg["top_ICD10_codes_path"]) as f:
         icd_codes = f.readlines()
@@ -24,12 +35,15 @@ def prepare_codes(cfg: Dict) -> None:
     icd_probs = icd_probs / icd_probs.sum()
     icd_codes_probs = dict(zip(icd_codes, icd_probs))
 
-    codes_filt = codes.query("Category_Code in @icd_codes")
+    codes_filt = codes.query("Primary_code in @icd_codes")
+
+    mask = codes_filt["Secondary_code"].apply(lambda x: len(x)) <= cfg["max_icd_depth"]
+    codes_filt = codes_filt[mask]
 
     codes = []
     probs = []
-    for cat in codes_filt["Category_Code"].unique():
-        subcats = codes_filt.query("Category_Code == @cat")["Diagnosis_Code"].values
+    for cat in codes_filt["Primary_code"].unique():
+        subcats = codes_filt.query("Primary_code == @cat")["Secondary_code"].values
         code_p = icd_codes_probs[cat]
         for subcat in subcats:
             codes.append(cat + "." + str(subcat) if str(subcat) != "nan" else cat)
