804ecffd589a268ec46a197f9b989b4acc16a7e9
nikolai <aby2sz@gmail.com>
2023-08-17 18:09:24 +0300

    token to download

--- self_instruct/download_models.sh
+++ self_instruct/download_models.sh
@@ -2,6 +2,7 @@ MODEL_DIR='models/decapoda-llama-7b'
 BASE_MODEL='decapoda-research/llama-7b-hf'
 LORA_DIR='models/ru_turbo_alpaca_7b'
 LORA_WEIGHTS='IlyaGusev/llama_7b_ru_turbo_alpaca_lora'
+HF_TOKEN=''
 
 docker run --name alpaca -v "${HOME}"/.cache:/root/.cache \
   -v "${HOME}"/MedTexts/alpaca-lora/"${MODEL_DIR}":/workspace/"${MODEL_DIR}" \
@@ -10,5 +11,7 @@ docker run --name alpaca -v "${HOME}"/.cache:/root/.cache \
   --env BASE_MODEL=$BASE_MODEL \
   --env LORA_DIR=$LORA_DIR \
   --env LORA_WEIGHTS=$LORA_WEIGHTS \
+  --env HF_TOKEN=HF_TOKEN \
+
   --rm alpaca-lora \
   python3.10 download_models.py
