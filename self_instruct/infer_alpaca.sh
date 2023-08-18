d96a448cdc8d1b53940a4f0833ac8fd1f61b3cc1
nikolai <aby2sz@gmail.com>
2023-08-18 10:33:59 +0300

    infer ru alpaca

--- self_instruct/infer_alpaca.sh
+++ self_instruct/infer_alpaca.sh
@@ -3,9 +3,10 @@
 docker run --gpus '"device=5"' --shm-size 64g -p 7860:7860 \
   -v "${HOME}"/cache:/root/.cache \
   -v "${HOME}"/SberMedText/self_instruct/data/:/workspace/data \
+  -v "${HOME}"/SberMedText/self_instruct/models/:/workspace/models \
   -v "${HOME}"/SberMedText/self_instruct/output:/workspace/output --rm alpaca-lora \
   python3.10 infer_alpaca.py \
-    --model_name 'models/decapoda-llama-7b' \
+    --model_name 'models/ru_llama_7b_lora' \
     --template_path 'templates/alpaca.json' \
     --input_path 'data/samples.json' \
     --output_path 'output/samples.json'
