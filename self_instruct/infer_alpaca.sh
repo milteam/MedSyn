1722ca6adeb5500f21ca40fdd73528429786af7f
nikolai <aby2sz@gmail.com>
2023-08-18 11:26:46 +0300

    infer ru alpaca

--- self_instruct/infer_alpaca.sh
+++ self_instruct/infer_alpaca.sh
@@ -4,8 +4,10 @@ docker run --gpus '"device=5"' --shm-size 64g -p 7860:7860 \
   -v "${HOME}"/cache:/root/.cache \
   -v "${HOME}"/SberMedText/self_instruct/data/:/workspace/data \
   -v "${HOME}"/SberMedText/self_instruct/models/:/workspace/models \
+  -v "${HOME}"/SberMedText/self_instruct/:/workspace/code \
+
   -v "${HOME}"/SberMedText/self_instruct/output:/workspace/output --rm alpaca-lora \
-  python3.10 infer_alpaca.py \
+  python3.10 code/infer_alpaca.py \
     --model_name 'IlyaGusev/llama_7b_ru_turbo_alpaca_lora' \
     --template_path 'templates/alpaca.json' \
     --input_path 'data/samples.json' \
