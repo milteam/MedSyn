def225cab93a410c8fcea96a18e79cdf34ac8e0a
Julia Kuzkina <juliakuzkina1@gmail.com>
2023-08-23 16:06:20 +0300

    finetuning: removed unused files

--- self_instruct/hf_generation.sh
+++
@@ -1,4 +0,0 @@
-docker run --gpus '"device=5"' --shm-size 64g -p 7860:7860 \
-  -v "${HOME}"/.cache:/root/.cache \
-  -v "${HOME}"/MedTexts/alpaca-lora/output:/workspace/output --rm alpaca-lora \
-  python3.10 hf_generation.py
