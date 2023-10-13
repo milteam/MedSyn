8b1bd11d6b5364ae17b02b93ca0603c5130ecabc
glebkaa <iamglebk@gmail.com>
2023-10-13 15:22:43 +0300

    almazov data and scripts added

---
+++ lib/data/data_ift/almazov2ift.py
@@ -0,0 +1,56 @@
+"""Convert Almazov anamnesis data to IFT data format."""
+
+from typing import Dict
+
+import os
+import re
+import random
+from tqdm import tqdm
+import pandas as pd
+import json
+import click
+
+
+INSTRUCTIONS = ["Допиши анамнез", "Продолжи анамнез"]
+
+
+def get_sample_by_continuation(text: str, ratio: int = 2) -> Dict:
+    instruction = random.choice(INSTRUCTIONS)
+
+    output = text.replace("\n", " ")
+    output = re.sub(" +", " ", output).split(" ")
+    pre = " ".join(output[: len(output) // ratio])
+    post = " ".join(output[len(output) // ratio :])
+
+    sample = {"instruction": instruction, "input": pre, "output": post}
+    return sample
+
+
+@click.command()
+@click.option("--results-dir", default="data/data_ift/almazov")
+@click.option("--result-name", default="almazov_data_ift.json")
+@click.option("--samples-path", default="data/data_raw/almazov.csv")
+def generate_data(results_dir: str, result_name: str, samples_path: str) -> None:
+    os.makedirs(results_dir, exist_ok=True)
+
+    result = []
+    samples = pd.read_csv(samples_path)
+
+    for idx in tqdm(samples.index):
+        text = samples.loc[idx]["text"]
+
+        new_sample = get_sample_by_continuation(text, ratio=2)
+        result.append(new_sample)
+
+        new_sample = get_sample_by_continuation(text, ratio=3)
+        result.append(new_sample)
+
+        new_sample = get_sample_by_continuation(text, ratio=4)
+        result.append(new_sample)
+
+    with open(os.path.join(results_dir, result_name), "w", encoding="utf8") as f:
+        json.dump(result, f, indent=3, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    generate_data()
