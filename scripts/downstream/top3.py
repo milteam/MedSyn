6bc6d50ebfdcd367368ce06b9d73a718025454e8
nikolai <aby2sz@gmail.com>
2023-10-20 15:39:34 +0300

    no sentence bert

--- scripts/downstream/top3.py
+++ scripts/downstream/top3.py
@@ -2,7 +2,6 @@ import click
 import torch
 
 from scripts.downstream.top3_model.hf import train_huggingface
-from scripts.downstream.top3_model.sentence_bert import train_sentence_transformers
 
 
 @click.command()
@@ -10,17 +9,12 @@ from scripts.downstream.top3_model.sentence_bert import train_sentence_transform
 @click.option("--val", "-v", help="Validation set jsonl file", required=True)
 @click.option("--pred", "-p", help="Predictions csv")
 @click.option("--checkpoint", "-c", help="Path to store model", required=True)
-@click.option('--model', '-m', type=click.Choice(['st', 'hf']),
-              help="st - sentence transformer, hf - huggingface model for sequence classication", required = True)
 @click.option("--bert", "-b", help="BERT base", required=True)
 @click.option("--epochs", "-e", help="Number of epochs", default=20)
-def main(train, val, pred, checkpoint, model, bert, epochs):
+def main(train, val, pred, checkpoint, bert, epochs):
     torch.seed()
 
-    if model == "st":
-        train_sentence_transformers(train, val, pred, checkpoint, bert, epochs)
-    elif model == "hf":
-        train_huggingface(train, val, pred, checkpoint, bert, epochs)
+    train_huggingface(train, val, pred, checkpoint, bert, epochs)
 
 
 if __name__ == '__main__':
