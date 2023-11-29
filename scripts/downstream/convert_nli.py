5bb0a3ec1c3f36333ba22737fec844082870cd45
nikolai <aby2sz@gmail.com>
2023-11-29 10:39:51 +0200

    combined dataset gpt4

--- scripts/downstream/convert_nli.py
+++ scripts/downstream/convert_nli.py
@@ -7,9 +7,12 @@ with open('scripts/downstream/data/RuMedNLI/train_ru_llama2_7b_ckpt_3.jsonl', 'w
     with open("scripts/downstream/data/RuMedNLI/ru_llama2_7b_ckpt_3_task_new_params.jsonl") as source:
         for row in source.readlines():
             row = json.loads(row)
+            if row["input"] is None or row["answer"] is None:
+                print("Skipping")
+                continue
             f.write(json.dumps(
                     {"idx": str(uuid.uuid4()),
-                     "ru_sentence1": row["input"],
+                     "ru_sentence_1": row["input"],
                      "ru_sentence_2": row["answer"],
                      "gold_label": row["gold_label"]}, ensure_ascii=False
                 ))
