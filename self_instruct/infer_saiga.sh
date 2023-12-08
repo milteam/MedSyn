ed75a9056fa6a5f40f2ce08e99b77c21ecf1fac0
Julia Kuzkina <juliakuzkina1@gmail.com>
2023-12-08 12:33:09 +0300

    updated infer_saiga

--- self_instruct/infer_saiga.sh
+++ self_instruct/infer_saiga.sh
@@ -1,7 +1,10 @@
 docker run --gpus '"device=5"' --shm-size 64g -p 7860:7860 \
--v "${HOME}"/.cache:/root/.cache \
--v "${HOME}"/MedTexts/alpaca-lora/output:/workspace/output --rm alpaca-lora \
-python3.10 infer_saiga.py \
+  -v "${HOME}"/.cache:/root/.cache \
+  -v "${PWD}"/data/:/workspace/data \
+  -v "${PWD}"/models/:/workspace/models \
+  -v "${PWD}"/:/workspace/ \
+  -v "${PWD}"/output:/workspace/output --rm alpaca-lora \
+  python3.10 infer_saiga.py \
   --model_name 'models/ru_saiga-v2_7b' \
   --template_path 'templates/saiga_v2.json' \
   --input_path 'test.jsonl' \
